dec7c7fe07cd28de18e8ed6883308bf6279d66e3
sym <1659455310@qq.com>
2024-06-23 17:45:39 +0800

    Site updated: 2024-06-23 17:45:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/21/test-1/","2024/03/07/GNU-Linux-shell/","2024/03/02/数组/","2024/04/24/hello-world/","2023/12/09/数据库复习/","2024/03/21/图像处理实验二/","2024/03/21/music-player/","2023/05/17/Java-GUI-音乐播放器/"];function toRandomPost(){
+var posts=["2023/05/17/Java-GUI-音乐播放器/","2024/03/21/music-player/","2023/12/09/数据库复习/","2024/03/07/GNU-Linux-shell/","2024/04/24/hello-world/","2024/03/21/test-1/","2024/03/21/图像处理实验二/","2024/03/02/数组/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
